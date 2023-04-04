dfa01d6141e3a4d53f24cb41077bf6729e29dc00
BlueSpeedsterYT <36098277+BlueSpeedsterYT@users.noreply.github.com>
2023-04-04 21:39:09 +0300

    Revert "Update Player.cpp"
    This reverts commit 6c0fd17cff8ed70209c984583834188e5e788dff.

--- Nexus/Player.cpp
+++ Nexus/Player.cpp
@@ -79,10 +79,10 @@ void LoadPlayerFromList(byte characterID, int playerID)
 
             if (characterID == p) {
                 GetFileInfo(&info);
-                //activePlayerCount += 1; dunno if it would work
+                activePlayerCount += 1;
+                CloseFile();
                 LoadPlayerAnimation(strBuf, playerID);
                 SetFileInfo(&info);
-                CloseFile();
             }
             FileRead(&strLen, 1);
             FileRead(&strBuf, strLen); // player name
